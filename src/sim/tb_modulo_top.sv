5c548ccdbdde685dc6f2ec0bbf6314c8d9c9b94f
Juan90ww <j.obando.3@estudiantec.cr>
2025-11-19 12:02:41 -0600

    Update tb_modulo_top.sv

--- src/sim/tb_modulo_top.sv
+++ src/sim/tb_modulo_top.sv
@@ -1,58 +1,66 @@
-module tb;
+module tb_divisor_7bit;
+
     timeunit 1ns; timeprecision 1ps;
 
     logic clk;
     logic rst_n;
-    logic [3:0] a2,a1,a0,b2,b1,b0;
-    logic load, start_conv, ready;
-    logic [3:0] d3,d2,d1,d0;
+    logic start;
+    logic [6:0] A, B;
 
-    // Clock 10ns (100 MHz)
-    initial clk = 0;
+    logic [6:0] Q, R;
+    logic done, busy;
+
+    // Clock 10ns
     always #5 clk = ~clk;
 
-    top_adder dut(
-        .clk(clk), .rst_n(rst_n),
-        .a2(a2), .a1(a1), .a0(a0),
-        .b2(b2), .b1(b1), .b0(b0),
-        .load(load), .start_conv(start_conv),
-        .out_d3(d3), .out_d2(d2), .out_d1(d1), .out_d0(d0),
-        .ready(ready)
+    divisor_7bit dut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .start(start),
+        .dividendo(A),
+        .divisor(B),
+        .cociente(Q),
+        .residuo(R),
+        .done(done),
+        .busy(busy)
     );
 
-    task do_test(input [3:0] A2,A1,A0,B2,B1,B0);
+    task do_test(input [6:0] a, input [6:0] b);
     begin
-        a2=A2; a1=A1; a0=A0;
-        b2=B2; b1=B1; b0=B0;
-        load = 1; @(posedge clk);
-        load = 0;
-        start_conv = 1; @(posedge clk);
-        start_conv = 0;
-        wait (ready);
-        $display("BCD out = %0d %0d %0d %0d", d3, d2, d1, d0);
+        A = a;
+        B = b;
+        start = 1;
+        @(posedge clk);
+        start = 0;
+
+        wait(done);
+        $display("DIVISION: %0d / %0d = COCIENTE %0d, RESIDUO %0d",
+                 a, b, Q, R);
         @(posedge clk);
     end
     endtask
 
     initial begin
-        $dumpfile("waves.vcd");
-        $dumpvars(0, tb);
+        $dumpfile("div_7bit.vcd");
+        $dumpvars(0, tb_divisor_7bit);
 
+        clk = 0;
         rst_n = 0;
-        load = 0; start_conv = 0;
+        start = 0;
         @(posedge clk);
         rst_n = 1;
         @(posedge clk);
 
-        $display("Test 1: 123 + 456 = 579");
-        do_test(1,2,3, 4,5,6);
+        $display("===== CASO 1: 50 / 5 =====");
+        do_test(50, 5);
 
-        $display("Test 2: 999 + 999 = 1998");
-        do_test(9,9,9, 9,9,9);
+        $display("===== CASO 2: 100 / 7 =====");
+        do_test(100, 7);
 
-        $display("Test 3: 007 + 015 = 0022");
-        do_test(0,0,7, 0,1,5);
+        $display("===== CASO 3: 127 / 3 =====");
+        do_test(127, 3);
 
         $finish;
     end
+
 endmodule
