b83e91dce67fa71335511c3fcc0c95fb9ad6844e
Juan90ww <j.obando.3@estudiantec.cr>
2025-11-27 15:20:59 -0600

    Update tb_top_divisor.sv

--- src/sim/tb_top_divisor.sv
+++ src/sim/tb_top_divisor.sv
@@ -5,15 +5,15 @@ module tb_top_divisor;
     logic clk = 0;
     logic rst = 0;
 
-    logic [3:0] fil;  // FILAS (entradas)
-    logic [3:0] col;  // COLUMNAS (salidas)
+    logic [3:0] fil;
+    logic [3:0] col;
 
     logic [3:0] anodo;
     logic [6:0] seven;
 
     always #5 clk = ~clk;
 
-    top_divisor dut (
+    top_divisor dut(
         .clk(clk),
         .rst(rst),
         .fil(fil),
@@ -24,25 +24,17 @@ module tb_top_divisor;
 
     initial begin
         $dumpfile("tb_top_divisor.vcd");
-        $dumpvars(0,tb_top_divisor);
+        $dumpvars(0, tb_top_divisor);
 
         rst = 0;
-        fil = 4'b1111;   // sin presionar tecla
+        fil = 4'b1111;
         #50 rst = 1;
 
-        // ============================
-        // Simular ingreso de A = 0x7F
-        // ============================
+        press_key(7);  // A = 0x7F
+        press_key(15);
 
-        press_key(4'b0111); // 7
-        press_key(4'b1110); // F
-
-        // ============================
-        // Simular ingreso de B = 0x05
-        // ============================
-
-        press_key(4'b0101); // 5
-        press_key(4'b0000); // *
+        press_key(5);  // B = 0x50
+        press_key(0);
 
         #200000;
 
@@ -50,14 +42,27 @@ module tb_top_divisor;
         $finish;
     end
 
-    // Simula una tecla presionada durante 2 ms
-    task press_key(input logic [3:0] key);
+    // modelo para keypad
+    task press_key(input int hex);
         begin
-            fil = key;
-            #(2000); // tiempo presionada
-            fil = 4'b1111; // liberar tecla
-            #(2000);
+            repeat(5000) begin
+                @(posedge clk);
+                fil = fila_por_hex(hex, col);
+            end
+            fil = 4'b1111;
+            repeat(2000) @(posedge clk);
         end
     endtask
+
+    // traduce un valor hex a una fila válida según columna activa
+    function logic [3:0] fila_por_hex(input int val, input logic [3:0] col);
+        case (col)
+            4'b0111: fila_por_hex = (val==1)?4'b0111:(val==4)?4'b1011:(val==7)?4'b1101:(val==14)?4'b1110:4'b1111;
+            4'b1011: fila_por_hex = (val==2)?4'b0111:(val==5)?4'b1011:(val==8)?4'b1101:(val==0)?4'b1110:4'b1111;
+            4'b1101: fila_por_hex = (val==3)?4'b0111:(val==6)?4'b1011:(val==9)?4'b1101:(val==15)?4'b1110:4'b1111;
+            4'b1110: fila_por_hex = (val==10)?4'b0111:(val==11)?4'b1011:(val==12)?4'b1101:(val==13)?4'b1110:4'b1111;
+            default: fila_por_hex = 4'b1111;
+        endcase
+    endfunction
 
 endmodule
