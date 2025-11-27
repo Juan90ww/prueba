876501c63f64d677f44a78d720b4c6e10fb38847
Juan90ww <j.obando.3@estudiantec.cr>
2025-11-27 04:12:49 -0600

    Update tb_top_divisor.sv

--- src/sim/tb_top_divisor.sv
+++ src/sim/tb_top_divisor.sv
@@ -1,44 +1,49 @@
+`timescale 1ns/1ps
+
 module tb_top_divisor;
 
     logic clk, rst;
     logic [3:0] fil;
-    wire  [3:0] col;
-    wire [3:0] anodo;
-    wire [6:0] seven;
+    logic [3:0] col;
+    logic [3:0] anodo;
+    logic [6:0] seven;
 
     top_divisor dut(
         .clk(clk), .rst(rst),
-        .fil(fil),
-        .col(col),
+        .fil(fil), .col(col),
         .anodo(anodo), .seven(seven)
     );
 
-    initial clk = 0;
     always #5 clk = ~clk;
 
-    task tecla(input logic [3:0] row);
-        begin
-            fil = row;
-            repeat(20000) @(posedge clk);
-            fil = 4'b1111;
-            repeat(20000) @(posedge clk);
-        end
+    task press(input [3:0] fila, input [3:0] tecla_hex);
+        fil = fila;
+        repeat(5) @(posedge clk);
+        fil = 4'b1111;
+        repeat(5) @(posedge clk);
     endtask
 
     initial begin
-        rst = 0; fil = 4'b1111;
+        $dumpfile("tb_top_divisor.vcd");
+        $dumpvars(0, tb_top_divisor);
+
+        clk=0; rst=0; fil=4'b1111;
         repeat(5) @(posedge clk);
-        rst = 1;
+        rst=1;
 
-        // Simular A = 42h, B = 08h
-        tecla(4'b0111); // tecla 4
-        tecla(4'b1011); // tecla 2
-        tecla(4'b0111); // tecla 0
-        tecla(4'b1101); // tecla 8
+        // A = 45 → tecla 4 y 5
+        press(4'b1011, 4'h4);
+        press(4'b1011, 4'h5);
 
-        repeat(500000) @(posedge clk);
+        // B = 12 → tecla 1 y 2
+        press(4'b0111, 4'h1);
+        press(4'b0111, 4'h2);
 
+        repeat(2000) @(posedge clk);
+
+        $display("Simulación TOP completa");
         $finish;
     end
 
 endmodule
+
